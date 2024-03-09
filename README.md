19f6cacee74355ac331f92bd2f41e377b33f9987
Peter-Roh <minchul.roh.peter@gmail.com>
2024-03-09 19:00:12 +0900

    learning rust by making a cli app

---
+++ README.md
@@ -0,0 +1,7 @@
+# grrs
+
+learning rust by making a cli app
+
+## references
+
+https://rust-cli.github.io/book/index.html
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    pattern: String,
+
+    path: std::path::PathBuf,
+}
+
+#[derive(Debug)]
+struct FileErr(String);
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
 }
